632de3832ab701539dd26af79a6aff37b872fafb
Sai Seng Sint <stevensint2020@gmail.com>
2025-10-14 23:09:41 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+books = [
+    {"id": 1, "title": "To Kill a Mockingbird", "author": "Harper Lee", "year": 1960},
+    {"id": 2, "title": "1984", "author": "George Orwell", "year": 1949},
+    {"id": 3, "title": "The Great Gatsby", "author": "F. Scott Fitzgerald", "year": 1925},
+    {"id": 4, "title": "Pride and Prejudice", "author": "Jane Austen", "year": 1813},
+    {"id": 5, "title": "The Catcher in the Rye", "author": "J.D. Salinger", "year": 1951}
+]
+
+@app.get('/')
+def read_root():
+    return {"Hello" : "World"}\
+
+# get all books
+@app.get('/books')
+def getAllBooks():
+    return books
